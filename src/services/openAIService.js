01341154c12831a2d62103b04e4ba06946aa5021
Adrian Mørbán <46272302+adrianmorban@users.noreply.github.com>
2024-09-18 17:49:04 -0400

    Refactor openAIService.js to update model for openAICompletion function

--- src/services/openAIService.js
+++ src/services/openAIService.js
@@ -2,30 +2,32 @@ import OpenAI from 'openai';
 import { setAppointment } from '../db/dynamoDB.js';
 
 const setAppointmentCalling = {
-  "name": "set_appointment",
-  "description": "Set the appoinment for the client. Call this everytime you know the client wants to set an appoinment, for example when a customer says 'I want to set an appoinment'.",
-  "parameters": {
-      "type": "object",
-      "properties": {
-        day: {
-          type: "string",
-          description: "The day of the appoinment in the format YYYY-MM-DD, if the today is greater than the day of the appoinment, the appoinment will be set for the next day.",
+  type: "function",
+  function: {
+    name: "set_appointment",
+    description: "Set the appoinment for the client. Call this everytime you know the client wants to set an appoinment, for example when a customer says 'I want to set an appoinment'.",
+    parameters: {
+        type: "object",
+        properties: {
+          day: {
+            type: "string",
+            description: "The day of the appoinment in the format YYYY-MM-DD, if the today is greater than the day of the appoinment, the appoinment will be set for the next day.",
+          },
+          hour: {
+            type: "string",
+            description: "The hour of the appoinment in the format HH:MM.",
+          },
+          fullName: {
+            type: "string",
+            description: "The full name of the client.",
+          },
+          dni: {
+            type: "string",
+            description: "The DNI of the client, it must be a string in the format xxx-xxxxxxx-x where x is a number and can be without the -.",
+          }
         },
-        hour: {
-          type: "string",
-          description: "The hour of the appoinment in the format HH:MM.",
-        },
-        fullName: {
-          type: "string",
-          description: "The full name of the client.",
-        },
-        dni: {
-          type: "string",
-          description: "The DNI of the client, it must be a string in the format xxx-xxxxxxx-x where x is a number and can be without the -.",
-        }
-      },
-      "required": ["day", "hour", "fullName", "dni"],
-      "additionalProperties": false,
+        required: ["day", "hour", "fullName", "dni"],
+    }
   }
 }
 
@@ -34,6 +36,7 @@ const client = new OpenAI({
 });
 
 export const openAICompletion = async (messages) => {
+
   let messageToOpenAi = [
     {
       role: 'system', 
@@ -45,7 +48,7 @@ export const openAICompletion = async (messages) => {
     model: 'ft:gpt-4o-2024-08-06:personal:sally:A8t20vlA',
     messages: messageToOpenAi,
     tools: [setAppointmentCalling],
-    tools_choice: "auto"
+    tool_choice: "auto",
   });
 
   if(completion.choices[0].tool_call === "set_appointment"){
